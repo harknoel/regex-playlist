60abf9d09799051d52e960d99ad5d6f93beedfc5
harknoel <bontilao.harknoel@gmail.com>
2023-01-19 02:23:34 +0800

    Add slug regEx

--- validation.js
+++ validation.js
@@ -4,7 +4,8 @@ const inputs = document.querySelectorAll("input");
 const patterns = {
     telephone: /^\d{11}$/,
     username: /^[a-z\d]{5,12}$/i,
-    password: /^[\w@-]{8,20}$/
+    password: /^[\w@-]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/
 };
 
 // validation function
