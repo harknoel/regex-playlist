d5c2a779affca7664eb660e8ba8a1e626d18d53a
harknoel <bontilao.harknoel@gmail.com>
2023-01-19 01:55:56 +0800

    Add a forEach for inputs

--- validation.js
+++ validation.js
@@ -1,4 +1,12 @@
 // validation script here
+const inputs = document.querySelectorAll("input");
+
 const patterns = {
     telephone: /^\d{11}$/
 };
+
+inputs.forEach((input) => {
+    input.addEventListener("keyup", (e) => {
+        console.log(e.target.attributes.name.value);
+    });
+});
